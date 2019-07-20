6367333a4e8ba47bbd6372a7334941750ec5aea3
Crisimple <1240281595@qq.com>
2019-07-21 00:02:23 +0800

    132_python数据类型进阶(队列queue)

--- 01_DataType/13_advancedType.py
+++ 01_DataType/13_advancedType.py
@@ -44,6 +44,41 @@ class Stack():
         print(self.stack131)
 
 
+# 2.队列：FIFO(先进先出)
+class Queue():
+    def __init__(self, size):
+        self.queue132 = []
+        self.front = -1
+        self.rear = -1
+        self.size = size
+
+    def isfull(self):
+        return self.rear - self.front + 1 == self.size
+
+    def isempty(self):
+        return self.front == self.rear
+
+    # 入队列
+    def enqueue(self, x):
+        if self.isfull():
+            raise Exception("queue is full")
+        else:
+            self.queue132.append(x)
+            self.rear = self.rear + 1
+
+    # 出队列
+    def dequeue(self):
+        if self.isempty():
+            raise Exception("queue is empty")
+        else:
+            self.queue132.pop(0)
+            self.front = self.front + 1
+
+    def show_queue(self):
+        print(self.queue132)
+
+
+
 if __name__ == "__main__":
     s = Stack(10)
     s.show_stack()
@@ -54,3 +89,12 @@ if __name__ == "__main__":
         s.pop_statck()
     s.show_stack()
     print("stack FILO is end.......")
+
+    q = Queue(7)
+    q.show_queue()
+    for i in range(6):
+        q.enqueue(i)
+    q.show_queue()
+    for i in range(3):
+        q.dequeue()
+    q.show_queue()
