47da104cc4ff088b3472b9a7d533f3ae70df6fe0
crisimple <crisimple@foxmail.com>
2019-08-15 19:30:50 +0800

    rename file and 052_exceptionHanding---try...exception...else

--- 05_Function/52_exceptionHanding.py
+++ 05_Function/52_exceptionHanding.py
@@ -16,10 +16,25 @@ list_exception = [1, 2, 3]
 try:
     for i in list_exception:
         print(list_exception[i+2])
+
+    raise TypeError('....主动抛出异常...')
 except IndexError as e:
     print("异常 %s 已经在这里被捕获了，下面再有异常处理就不再被捕获了" % e)
 except Exception as e:
     print(e)
 finally:
-    print("程序执行结束，不管异常是否被捕获，这里一定始终会执行")
+    print("程序执行结束，不管异常是否被捕获，这里一定始终会执行\n")
+
+
+# 2. try...exception...else
+else_exception = [1, 2, 3]
+try:
+    for i in [1, 4]:
+        print(i)
+except IndexError as e:
+    print(e)
+except SyntaxError as se:
+    print(se)
+else:
+    print("只有再try语句里面的执行成功的时候，才会执行else里面的内容")
 
