05e130044835f46587a69018478d2ab1cbdc400e
crisimple <crisimple@foxmail.com>
2019-10-22 18:55:09 +0800

    11_DataStructureAlgorithm.111_Introduction.01_abc.py

---
+++ tests/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+@File    :   __init__.py
+@Time    :   2019/10/21 18:35
+@Author  :   Crisimple
+@Github :    https://crisimple.github.io/
+@Contact :   Crisimple@foxmail.com
+@License :   (C)Copyright 2017-2019, Micro-Circle
+@Desc    :   该包下面是平时测试用的各类脚本集合包，没有什么鲜明的功能特点
+"""
