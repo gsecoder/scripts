93a881cd7372d80a282855c876c50f75c54a1350
crisimple <crisimple@foxmail.com>
2019-11-06 19:19:27 +0800

    NetEase add db

---
+++ app/NetEase/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+@File    :   __init__.py
+@Time    :   2019/11/6 14:27
+@Author  :   Crisimple
+@Github :    https://crisimple.github.io/
+@Contact :   Crisimple@foxmail.com
+@License :   (C)Copyright 2017-2019, Micro-Circle
+@Desc    :   None
+"""
