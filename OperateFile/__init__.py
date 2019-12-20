2eba3bb212208472e0d43124435949bacec9a372
姜恒 <jiangheng@qianxin.com>
2019-12-20 18:08:42 +0800

    02_workpath_pathlib.py

---
+++ OperateFile/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+@File    :   __init__.py
+@Time    :   2019/12/2010:23
+@Author  :   Crisimple
+@Github :    https://crisimple.github.io/
+@Contact :   Crisimple@foxmail.com
+@License :   (C)Copyright 2017-2019, Micro-Circle
+@Desc    :   None
+"""
