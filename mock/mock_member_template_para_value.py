0ac1e9ab09fa354579e0733f313266d6c90e5661
姜恒 <jiangheng@qianxin.com>
2019-12-24 18:18:03 +0800

    mock/

---
+++ mock/mock_member_template_para_value.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+@File    :   mock_member_template_para_value.py
+@Time    :   2019/12/2416:34
+@Author  :   Crisimple
+@Github :    https://crisimple.github.io/
+@Contact :   Crisimple@foxmail.com
+@License :   (C)Copyright 2017-2019, Micro-Circle
+@Desc    :   None
+"""
+
+from mock.template_class import Template
+from unittest import mock
+import unittest
+
+
+class TemplateTest(unittest.TestCase):
+    
+    @unittest.skip("先跳吧")
+    def test_get_full_name(self):
+        t = Template()
+        
+        values = {
+            ('cris', 'simple'): 'cris simple',
+            ('xiao', 'ming'): 'xiao ming'
+        }
+        
+        t.get_full_name = mock.Mock(side_effect=lambda x, y: values[(x, y)])
+        self.assertEqual(t.get_full_name('cris', 'simple'), 'cris simple')
+        self.assertEqual(t.get_full_name('xiao', 'ming'), 'xiao ming')
+        
+    def test_raise_get_age(self):
+        t = Template()
+        t.get_age = mock.Mock(side_effect=TypeError("integer type"))
+        self.assertRaises(TypeError, t.get_age)
+        
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ mock/mock_member_template_side_effect.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+@File    :   mock_member_template_side_effect.py
+@Time    :   2019/12/2416:15
+@Author  :   Crisimple
+@Github :    https://crisimple.github.io/
+@Contact :   Crisimple@foxmail.com
+@License :   (C)Copyright 2017-2019, Micro-Circle
+@Desc    :   None
+"""
+
+from mock.template_class import Template
+import unittest
+from unittest import mock
+
+
+class TemplateTest(unittest.TestCase):
+    
+    def test_get_age(self):
+        t = Template()
+        t.get_age = mock.Mock(side_effect=[10, 11, 12])
+        
+        self.assertEqual(t.get_age(), 10)
+        self.assertEqual(t.get_age(), 11)
+        self.assertEqual(t.get_age(), 12)
+        
+
+if __name__ == "__main__":
+    unittest.main()
