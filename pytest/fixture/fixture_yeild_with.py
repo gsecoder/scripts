25f263841712e73b41c3a98de26c40b3a72329d2
crsimple <crisimple@foxmail.com>
2020-05-23 21:01:51 +0800

    unittest_api env init

--- pytest/fixture/fixture_yeild_with.py
+++
@@ -1,6 +0,0 @@
-import pytest
-
-@pytest.fixture(scope="module")
-def smtp_connection():
-    with smtp.SMTP("smtp.gmail.com", 587, timeout=5) as smtp_connection:
-        yield smtp_connection  # provide the fixture value
--- pytest/plug/pytest_rerunfailures.py
+++
@@ -1,7 +0,0 @@
-"""重新运行指定的测试用例"""
-import pytest
-import random
-
-@pytest.mark.flasky(reruns=5)
-def test_example():
-    assert random.choice([True, False, False])
