98c4ca052bbac001ede0ed092f3d73f74960e9c4
crsimple <crisimple@foxmail.com>
2020-06-02 00:52:34 +0800

    operate db and daymic create db

--- unittest_api/util/operate_database.py
+++ unittest_api/util/operate_database.py
@@ -12,4 +12,26 @@ import pymysql
 class OperateDatabase(object):
 
     def __init__(self):
-        self.connect = pymysql.connect()
+        self.connect = pymysql.connect(
+            host='129.28.170.125',
+            user='root',
+            passwd='159357',
+            port=3306,
+            charset='utf8'
+        )
+        self.cursor = self.connect.cursor()
+
+    def create_db(self, db_name):
+        try:
+            self.cursor.execute(
+                'CREATE DATABASE IF NOT EXISTS %s' % db_name
+            )
+            self.connect.select_db(db_name)
+            self.connect.commit()
+        except pymysql.DatabaseError as e:
+            print(e.args)
+
+
+if __name__ == "__main__":
+    odb = OperateDatabase()
+    odb.create_db('unittest_api')
