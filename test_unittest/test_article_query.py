a75a64adefa7b62ef2b3dd30eee58b19ddb59613
crsimple <crisimple@foxmail.com>
2020-06-06 11:13:35 +0800

    测试代码同步脚本

---
+++ test_unittest/test_article_query.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+__file__    : test_article_query.py
+__time__    : 2020/6/6 10:19
+__author__  : crisimple
+__github__ :  https://crisimple.github.io/
+"""
+
+import unittest
+
+class TestArticleQuery(unittest.TestCase):
+
+    def setUp(self) -> None:
+        pass
+
+    def test_article_query(self):
+        pass
+
+    def tearDown(self) -> None:
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ tools/push_code.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# 查看当前的 git 分支并获取分支名称
+current_branch_info=`git status | grep branch`
+current_branch=${current_branch_info:10}
+
+# 判断当前分支是否在 ${choice_branch} 分支上，如果不在的话进行分支的切换
+if [ "${current_branch}" == "${choice_branch}" ]
+then
+  echo "当前分支为：${current_branch}"
+else
+  git checkout unittest_api
+fi
+
+# 提交代码
+git add .
+git commit -m "测试代码同步脚本"
+git push origin "${current_branch}"
