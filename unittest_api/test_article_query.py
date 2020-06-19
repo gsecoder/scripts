789c247bd4e217c6000e7f109d85ad216d6a83c8
unknown <jiangheng@ESG.360ES.CN>
2020-06-19 18:10:17 +0800

    slove confilict

--- unittest_api/test_article_query.py
+++ unittest_api/test_article_query.py
@@ -1,59 +1,99 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 """
-__file__   : test_article_query.py 
-__author__ : jiangheng
-__date__   : 2020-06-05 9:27
-__function__:
-    文章接口查询测试
+__file__    : test_article_query_devtest.py
+__time__    : 2020/6/6 10:19
+__author__  : crisimple
+__github__ :  https://crisimple.github.io/
 """
 
 import unittest
-from util.read_ini import ReadIni
-from util.read_sql import ReadSql
 from util.request_method import RequestMethod
 from util.format_data import FormatData
+from util.read_ini import ReadIni
 from util.read_sql import ReadSql
+import json
+from util.compare_json import CompareJson
 
 class TestArticleQuery(unittest.TestCase):
 
-    def setUp(self):
-        # 初始化时从 unittest_api.ini 文件中读取被测接口的配置文件
-        ri = ReadIni(ini_file="../data/unittest_api.ini")
-        self.base_url = ri.read_ini('article_api', 'base_url')
-        api_headers_ini = ri.read_ini('article_api', 'article_headers')
+    @classmethod
+    def setUpClass(cls) -> None:
+        # 初始化格式化
+        cls.fd = FormatData()
 
-        # 初始化数据格式化工具，因为从ini配置文件中读取的 headers 为字符串形式，没有办法取得 item 的值
-        self.fd = FormatData
-        self.api_headers = self.fd.strdict_to_dict(api_headers_ini)
+        # 初始化读取 ini 文件方法
+        cls.ri = ReadIni(ini_file="../data/unittest_api.ini")
+        # 获取要请求接口的 url 和 headers
+        cls.base_request_type = cls.ri.read_ini("article_api_query", "base_request_type")
+        # print("base_request_type: ", self.base_request_type)
+        cls.base_url = cls.ri.read_ini("article_api_query", "base_url")
+        # print("base_url: ", self.base_url)
+        cls.base_headers = cls.fd.strdict_to_dict(cls.ri.read_ini("article_api_query", "base_headers"))
 
-        # 初始化时从 mysql.json 文件中读取被测接口的传参即测试用例数据
-        # 由于插叙接口不用传参所以不用初始化
+        # 初始化综合请求接口方法
+        cls.rm = RequestMethod()
 
-        # 初始化请求接口方法
-        self.rm = RequestMethod()
+        # 初始化执行SQL
+        cls.rs = ReadSql()
+        # self.rs = ReadSql(json_file="../data/sql.json")
 
-        # 初始化执行 SQL 操作方法
-        self.rs = ReadSql()
+        # 初始化比较返回结果的方法
+        cls.cj = CompareJson
 
-    def test_article_query(self):
+    def test_query_all(self):
 
-        # 请求接口返回的值
+        # 接口传参获取接口数据，鉴于 article_query 接口不用传参，所以就不用获取参数了
         api_data = self.rm.request_method(
-            request_type='GET',
-            api_url=self.base_url + 'query/',
-            headers=self.api_headers
+            request_type=self.base_request_type,
+            api_url=self.base_url + "query/",
+            headers=self.base_headers,
         )
-        result_text = api_data.text.encode('utf-8').decode('unicode_escape')
-        print("type of result_text: ", result_text)
+        query_data_code = api_data.status_code
+        query_data_text = json.loads(api_data.text.encode("utf-8").decode("unicode_escape"))
+        api_data_text = query_data_text["articles"]
 
-        # 请求数据库获取返回值
-        sql_data = self.rs.select_sql('select')
+        # print("query_data_code: ", query_data_code)
+        # print("query_data_text: ", api_data_text)
+        # return api_data
+
+        # 传参读取数据库中的数据
+        sql_data = self.rs.select_sql("select", "query_all")
+        # sql_data_dict = sql_data["第一"]
+        # print("sql_data: ", type(sql_data))
+        print("sql_data: ", sql_data)
+
+        # 比较 api 返回数据与 sql 查询逻辑返回数据对比
+        # self.cj.compare_json(api_data_text, sql_data_dict)
+        self.assertEqual(api_data_text, sql_data, "相等")
+
+    def test_query_condition(self):
+        # 接口传参获取接口数据，鉴于 article_query 接口不用传参，所以就不用获取参数了
+        api_data = self.rm.request_method(
+            request_type=self.base_request_type,
+            api_url=self.base_url + "query/",
+            headers=self.base_headers
+        )
+        query_data_text = json.loads(api_data.text.encode("utf-8").decode("unicode_escape"))
+        api_data_text = query_data_text["articles"]
+        print(api_data_text)
+
+        # 传参读取数据库中的数据
+        sql_data = self.rs.select_sql(method="select", which_args="query_condition")
         print(sql_data)
 
-def tearDown(self):
-        pass
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        print("执行完毕")
 
 
 if __name__ == "__main__":
-    unittest.main()
+    # 构建测试套件
+    suite = unittest.TestSuite()
+    suite.addTest(TestArticleQuery('test_query_all'))
+    suite.addTest(TestArticleQuery('test_query_condition'))
+
+    # 执行测试
+    runner = unittest.TextTestRunner(verbosity=2)
+    runner.run(suite)
--- util/format_data.py
+++ util/format_data.py
@@ -25,20 +25,16 @@ class FormatData(object):
         )
         return strtuple
 
-<<<<<<< HEAD
     def strdict_to_dict(b):
         # print("type of b", type(b))
         dict_info = json.loads(b)
         # print("type of temp: ", type(dict_info))
         return dict_info
-=======
+
     @staticmethod
     def strdict_to_dict(b):
         temp = json.loads(b)
         return temp
-
->>>>>>> 9195aa4c97d9c0804db903427b80055c54654196
-
 
 if __name__ == "__main__":
     fd = FormatData
@@ -48,13 +44,10 @@ if __name__ == "__main__":
     # print(fd.strtuple_to_tuple(aa))
     # print("type of fd.strtuple_to_tuple(aa)", type(fd.strtuple_to_tuple(aa)))
 
-<<<<<<< HEAD
     bb = '{"a": 22, "b": 23}'
     print(fd.strdict_to_dict(bb))
-=======
     bb = '{"aa":11, "bb":22}'
     fd_dict = fd.strdict_to_dict(bb)
     print("type of fd_dict: ", type(fd_dict))
     print("fd_dict: ", fd_dict)
->>>>>>> 9195aa4c97d9c0804db903427b80055c54654196
 
