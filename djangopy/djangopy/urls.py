12626f18318c4f9444668c7b864386d4a957ac5f
crsimple <crisimple@foxmail.com>
2020-06-25 17:03:59 +0800

    renew djangopy project

---
+++ djangopy/djangopy/urls.py
@@ -0,0 +1,26 @@
+"""djangopy URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+import os
+from django.contrib import admin
+from django.urls import path
+from django.urls import include
+
+os.environ.update({"DJANGO_SETTINGS_MODULE": "config.settings"})
+
+urlpatterns = [
+    path('blog/', include('blog.urls')),
+    path('admin/', admin.site.urls),
+]
